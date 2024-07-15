1ffad41d79b76a91a8a9f320b20801ef26b6f235
client000 <comp04272@gmail.com>
2024-07-15 22:17:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
 from typing import Optional
 from fastapi import FastAPI, Request, BackgroundTasks
 import httpx
+import uvicorn
 
 app = FastAPI()
 
-async def reply(payload):
-    print("here")
+async def reply(request: Request):
     url = "https://client0001.pythonanywhere.com/wati_whatsapp"
-
     try:
-        print("Are you there")
+        body = await request.json()
         async with httpx.AsyncClient() as client:
-            response = await client.post(url, json=payload)
-            print("Request sent")
+            await client.post(url, json=body)
     except Exception as e:
-        print("request failed:", e)
+        print("There was error sending the request")
+    return {"message": "Success!"}
 
 @app.post("/whatsapp")
 async def root(request: Request, background_tasks: BackgroundTasks):
-    # Parse incoming request JSON
-    body = await request.json()
-    
-    question = str(body.get("text", ""))
-    contactnum = str(body.get("waId", "errresponse"))
-    sendername = str(body.get("senderName", ""))
-    
-    background_tasks.add_task(reply, body)
-
-    return "Success!"
+    background_tasks.add_task(reply, request)
+    return {"message": "Success!"}
     
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == '__main__':
-    import uvicorn
     uvicorn.run(app)
